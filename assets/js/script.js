47d5df68e8f1309ce10a080fdf4fead31673b6bd
mohamedabdalwahab <135446918+mohamedabdalwahab@users.noreply.github.com>
2024-06-07 23:25:23 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,63 @@
-'use strict';
-
 /**
- * navbar toggle
- */
-
-const overlay = document.querySelector("[data-overlay]");
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-const navLinks = document.querySelectorAll("[data-nav-link]");
-
-const navElemArr = [navOpenBtn, navCloseBtn, overlay];
-
-const navToggleEvent = function (elem) {
-  for (let i = 0; i < elem.length; i++) {
-    elem[i].addEventListener("click", function () {
-      navbar.classList.toggle("active");
-      overlay.classList.toggle("active");
+   * Hero type effect
+   */
+    let n;
+  document.addEventListener('DOMContentLoaded',HeroTypeEffect);
+ let relod = document.getElementById('relod');
+function HeroTypeEffect(){
+    if (n) n.destroy(); // التحقق من أن n معرف قبل استدعاء destroy
+ let typed = document.querySelector('.typed');
+if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(';');
+     n = new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
     });
-  }
 }
 
-navToggleEvent(navElemArr);
-navToggleEvent(navLinks);
+};
+relod.addEventListener('click', HeroTypeEffect);
+// -----------------------------------------------
+//------------------------------------------------
+// steart booton go up 
+let up = document.querySelector(".toUp");
 
+window.onscroll = function () {
+    if (window.scrollY >= 281.25) {
+        up.classList.add("show");
+    } else {
+        up.classList.remove("show");
+    }
+};
 
+up.onclick = function () {
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth",
+    });
+};
+// and booton go up
 
-/**
- * header sticky & go to top
- */
-
-const header = document.querySelector("[data-header]");
-const goTopBtn = document.querySelector("[data-go-top]");
-
-window.addEventListener("scroll", function () {
-
-  if (window.scrollY >= 200) {
-    header.classList.add("active");
-    goTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    goTopBtn.classList.remove("active");
-  }
-
-});
+    /*<![CDATA[*/
+//     document.oncontextmenu = function (event) {
+//         if (
+//             event.target.tagName != "INPUT" &&
+//             event.target.tagName != "TEXTAREA"
+//         ) {
+//             event.preventDefault();
+//         }
+//     };
+// document.ondragstart = function () {
+//     if (
+//         event.target.tagName != "INPUT" &&
+//         event.target.tagName != "TEXTAREA"
+//     ) {
+//         event.preventDefault();
+//     }
+// };
+      /*]]>*/
